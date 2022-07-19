c65fd8fa1549d70c3c9b66dc56810571c1bc3372
andres2202 <andresfelipenf@gmail.com>
2022-07-18 20:57:42 -0500

    deployment changes

---
+++ bankApp/views/userDetailView.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework.permissions import IsAuthenticated
+
+from bankApp.models.user import User
+from bankApp.serializers.userSerializer import UserSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        TokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token, verify=false)
+
+        if valid_data['user_id'] != kwargs['pk']:
+            stringresponse = {'detail':'Unauthorized Request'}
+            return Response(stringresponse, status=status.HTTP_401_UNAUTHORIZED)
+
+        return super().get(request,*args,**kwargs)
